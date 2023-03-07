fc57aaaf9d31beaa74a88b42cebfdeb795fc1caf
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-03-07 20:16:09 -0300

    Project created and recaptchav3

---
+++ src/main/java/com/starking/recaptchav3/Recaptchav3Application.java
@@ -0,0 +1,13 @@
+package com.starking.recaptchav3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recaptchav3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Recaptchav3Application.class, args);
+	}
+
+}
